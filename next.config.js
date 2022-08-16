09332b4041b5e8beb8a0fac11c173d66a69b6970
June <s982535@gmail.com>
2022-08-16 14:40:32 +0800

    feat: localhost hotReload, github action to github page demo

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  output: 'standalone',
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 }
