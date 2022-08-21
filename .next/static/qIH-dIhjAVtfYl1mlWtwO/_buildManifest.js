4fe3f4d9ac38acdd0a11e0084b09b12443ee981e
june50232 <s982535@gmail.com>
2022-08-21 03:58:58 +0000

    Deploying to gh-pages from @ june50232/AC-Re-React@f398a39d2394873304e4875868858d9e908c6794 🚀

--- .next/static/qIH-dIhjAVtfYl1mlWtwO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/92ff1f22dd9892a3.css","static/chunks/pages/index-0e51c36a88e1da86.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
